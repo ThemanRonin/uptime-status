32834c55ac506e05374748a3f52b9b1b3f1b4f3d
themanronin <35099500+ThemanRonin@users.noreply.github.com>
2024-07-17 09:08:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'ZGQ Inc. の 站点监测',
+  SiteName: '站点监测',
 
   // UptimeRobot API Keys
   // 支持Monitor-Specific API Keys和Read-Only API Key，推荐使用Read-Only API Key，可填写多个
@@ -16,22 +16,22 @@ window.Config = {
   ShowLink: true,
 
   // 导航栏菜单
-  Navi: [
-    {
-      text: '个人主页',
-      url: 'https://domain.zgqinc.gq/'
-    },
-    {
-      text: '小众软件',
-      url: 'https://meta.appinn.net/'
-    },    
-    {
-      text: '项目地址',
-      url: 'https://github.com/ZGQ-inc/uptime-status'
-    },
-    {
-      text: '原项目地址',
-      url: 'https://github.com/imsyy/status'
-    },
-  ],
+//  Navi: [
+  //  {
+   //   text: '个人主页',
+   //   url: 'https://domain.zgqinc.gq/'
+  //  },
+  //  {
+   //   text: '小众软件',
+   //   url: 'https://meta.appinn.net/'
+   // },    
+   // {
+   //   text: '项目地址',
+   //   url: 'https://github.com/ZGQ-inc/uptime-status'
+  //  },
+  //  {
+    //  text: '原项目地址',
+   //   url: 'https://github.com/imsyy/status'
+  //  },
+ // ],
 };
